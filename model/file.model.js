6b471f68b465e402dc65c894a45d35771b0720f6
Bhavesh-0909 <bhaveshchoudhary0909@gmail.com>
2024-01-26 20:43:19 +0530

    mail functionality added

--- model/file.model.js
+++ model/file.model.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const transportor = require('../db/nodemailer.db')
 
 const fileSchema = new mongoose.Schema({
     name:{
@@ -12,5 +13,21 @@ const fileSchema = new mongoose.Schema({
         type:String
     }
 }, {timestamps:true});
+
+fileSchema.post('save', function(doc) {
+    try {
+        let info = transportor.sendMail({
+            from:"Bhavesh",
+            to:doc.email,
+            subject:"File Upload Successfully",
+            html:`<h2>File Uploaded</h2>
+            <p>Thanks for using our services</p>
+            <p>View your image here :<a href=${doc.imageUrl}>${doc.imageUrl}</a></p>`
+        })
+    } catch (error) {
+        console.log(error)
+    }
+    
+})
 
 module.exports = mongoose.model("File", fileSchema)
